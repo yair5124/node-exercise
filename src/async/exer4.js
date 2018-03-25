e3b399cd030f4c42e2e76cc85018359616e66f53
Yair Rinot <yairinot@gmail.com>
2018-03-25 23:04:58 +0300

    More exercise

--- src/async/exer4.js
+++ src/async/exer4.js
@@ -1,15 +1,17 @@
 const async = require('async');
 const http = require('http');
 
-async.each(process.argv.slice(2), function(item, done) {
+async.map(process.argv.slice(2), function(item, done) {
     http.get(item, function(res) {
-        res.on('data', function(chunk) {});
+        var rawData = '';
+        res.on('data', function(chunk) { rawData += chunk; });
         res.on('end', function() {
-            done(null);
+            done(null, rawData);
         });
     }).on('error', function(e) {
         done(e);
     });
-    }, function(err){
+    }, function(err, results){
         if (err) console.error(err);
+        console.log(results);
 });
